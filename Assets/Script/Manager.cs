bfbe197f8dee3916a6368bf725568125a28a3ccd
JoJorge <jorge13jorge13@gmail.com>
2016-08-25 18:54:33 +0800

    first Manager try

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class StageInfo{
+	ObjectInfo[] objInfoes;	
+}
+public struct ObjectInfo {
+	string name;
+	Vector3 pos;
+}
+[System.Serializable]
+public struct StagePrefabs {
+	public string name;
+	public GameObject prfab;
+}
+
+public class Manager : MonoBehaviour {
+	public StagePrefabs[] dictionary;
+	StageInfo[] stageinfoes;
+	StageInfo[] stageRecord;
+	GameObject player;
+	int state;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
